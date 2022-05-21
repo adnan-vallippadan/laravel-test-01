ea60f6dd29bf5583746fc85eb95dbddd454d3e39
adnandop <adnanvpdop@gmail.com>
2022-05-21 16:12:30 +0530

    added migration files

--- database/migrations/2022_05_21_102420_create_user_data_tables.php
+++ database/migrations/2022_05_21_102420_create_user_data_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserDataTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_2', function (Blueprint $table) {
+        Schema::create('user_data', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('data_1');
+            $table->string('data_2');
+            $table->integer('data_3');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_data');
     }
 }
